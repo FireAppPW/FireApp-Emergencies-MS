6fcaf99f56662cecc397168bbc091cecf42e4139
FerGonzalez4 <fer4gonzalez@gmail.com>
2023-06-18 20:44:20 +0200

    Chat Backend

--- src/main/java/pw/ersms/emergencies/chat/KafkaConstants.java
+++
@@ -1,7 +0,0 @@
-package pw.ersms.emergencies.chat;
-
-public class KafkaConstants {
-    public static final Object KAFKA_BROKER = new Object();
-    public static final Object GROUP_ID = new Object();
-    public static final String KAFKA_TOPIC = "";
-}
