def56e4ec664111e23ffe58d4af3b8f5bf401aa8
FerGonzalez4 <fer4gonzalez@gmail.com>
2023-06-19 23:16:28 +0200

    Get emergencies policies

--- src/main/java/pw/ersms/emergencies/emergency/EmergencyController.java
+++ src/main/java/pw/ersms/emergencies/emergency/EmergencyController.java
@@ -4,8 +4,14 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.persistence.criteria.CriteriaBuilder;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.Principal;
 import java.util.List;
 
 @RestController
@@ -15,10 +21,6 @@ import java.util.List;
 public class EmergencyController {
     private final EmergencyService emergencyService;
 
-    @GetMapping
-    public List<Emergency> getEmergency() {
-        return emergencyService.get();
-    }
     @GetMapping(path = "/{departmentId}")
     public List<Emergency> getEmergency(@PathVariable("departmentId") Integer departmentId) {
         return emergencyService.get(departmentId);
@@ -27,6 +29,34 @@ public class EmergencyController {
     @GetMapping(path = "/{departmentId}/{id}")
     public Emergency getEmergencyById(@PathVariable("id") Integer id) {
         return emergencyService.getEmergencyById(id);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Emergency>> getEmergency(Principal principal) {
+        //get first element of userDetails.getAuthorities()
+        UserDetails userDetails = (UserDetails) ((UsernamePasswordAuthenticationToken) principal).getPrincipal();
+
+        //check if getAuthorities() returns a list of size 2
+        if (userDetails.getAuthorities().size() != 2) {
+            throw new BadCredentialsException("Invalid credentials");
+        }
+
+        //departmentId
+        System.out.println(userDetails.getAuthorities().toArray()[0]);
+        Integer departmentId = Integer.parseInt(userDetails.getAuthorities().toArray()[0].toString());
+
+        //role
+        System.out.println(userDetails.getAuthorities().toArray()[1]);
+        String role = userDetails.getAuthorities().toArray()[1].toString();
+
+        if (role.equals("SysAdmin")) {
+            return ResponseEntity.ok().body(emergencyService.get());
+        } else if (role.equals("FireAdmin")) {
+            return ResponseEntity.ok().body(emergencyService.get(departmentId));
+        } else {
+            //return unauthorized
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
     }
 
     @PostMapping
